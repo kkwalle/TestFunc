42438687b383b2816ecf8c1dd4a3c8cd6057fc2e
kkwalle <kksw1024@gmail.com>
2018-03-12 18:10:48 +0800

    add testB

---
+++ TestB.h
@@ -0,0 +1,13 @@
+//
+//  TestB.h
+//  TestFunc
+//
+//  Created by kkwalle on 2018/3/12.
+//  Copyright © 2018年 kkwalle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestB : NSObject
++ (void)TestB;
+@end
